6995a3aefc71e0eb2557876e31b6c2c1223c5739
LSapee <dlworhks94@gmail.com>
2025-04-21 15:57:06 +0900

    url 적용

--- store/useAuthStore.ts
+++ store/useAuthStore.ts
@@ -4,8 +4,7 @@ import { toast } from 'react-toastify';
 import {AuthState} from "@/interface/Auth.interface";
 import {io} from "socket.io-client";
 
-// const BASE_URL = "https://chatapi.lsapee.com/chat"
-const BASE_URL = process.env.CHAT_BASE_URL
+const BASE_URL = "https://chatapi.lsapee.com/chat"
 
 export const useAuthStore = create<AuthState>((set,get)=>({
     authUser:null,

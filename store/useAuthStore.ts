4a61c10f4d04bd60e042ee181ea34035836b7fbb
LSapee <dlworhks94@gmail.com>
2025-05-07 22:55:38 +0900

    소켓 url 로컬 > 배포 url

--- store/useAuthStore.ts
+++ store/useAuthStore.ts
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import {AuthState} from "@/interface/Auth.interface";
 import {io} from "socket.io-client";
 
-const BASE_URL = "https://chat.lsapee.com/chat"
+const BASE_URL = "https://chatapi.lsapee.com/chat"
 
 export const useAuthStore = create<AuthState>((set,get)=>({
     authUser:null,

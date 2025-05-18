cdc8a1ec97a9cf5d1029351488581dc624710a3a
LSapee <dlworhks94@gmail.com>
2025-05-19 00:17:44 +0900

    채팅 페이지 한글화

--- comporents/ChatRooms.tsx
+++ comporents/ChatRooms.tsx
@@ -18,7 +18,7 @@ const ChatRooms = () =>{
             <div className="border-b border-base-300 w-full p-5">
                 <div className="flex items-center gap-2">
                     <MessageSquare className ="2-6 h-6"/>
-                    <span className="font-medium hidden lg:block">Chats</span>
+                    <span className="font-medium hidden lg:block">채팅</span>
                 </div>
             </div>
             <div className="overflow-y-auto w-full py-3">
